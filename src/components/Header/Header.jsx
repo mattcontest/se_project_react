adaea2394bda9dfd7353cbc2e9f1c830c79e7575
menth0s <marslite@pm.me>
2025-01-09 11:49:35 +0100

    Implementing Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <img src={logo} className="header__logo" />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes">+Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance User</p>
+        <img src={avatar} alt="Terrence" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
