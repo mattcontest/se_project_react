e69b8b8234a2a42ea38fa181cb6e23298c8ee315
menth0s <marslite@pm.me>
2025-01-11 13:38:17 +0100

    Filtering out based unpon weather

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
-function Main() {
+import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
+
+function Main({ weatherData }) {
+  console.log("From main", weatherData);
   return (
     <main>
       <WeatherCard />
       <section className="cards">
         <p className="cards__text">Today is 75° F / You may want to wear:</p>
-        {/*Add Cards here*/}
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
