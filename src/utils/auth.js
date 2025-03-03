0571e72eb1072bec0dce17dbf84025dcaf6b61e1
menth0s <marslite@pm.me>
2025-03-03 19:24:28 +0100

    Implemented auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,35 @@
+import { checkResponse } from "./api";
+
+export const BASE_URL = "http://localhost:3001";
+
+export const registerUser = (userData) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: userData.name,
+      avatar: userData.avatar,
+      email: userData.email,
+      password: userData.password,
+    }),
+  }).then((res) => {
+    checkResponse(res);
+  });
+};
+
+export const loginUser = (userData) => {
+  return fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: userData.email,
+      password: userData.password,
+    }),
+  }).then((res) => {
+    checkResponse(res);
+  });
+};
