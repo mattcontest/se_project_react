e317d951b958ff3b35b05af86800d5003ecff4ed
menth0s <marslite@pm.me>
2025-03-03 18:42:47 +0100

    Styled Change Profile and Log Out buttons in /profile

--- src/components/SidebBar/SideBar.jsx
+++ src/components/SidebBar/SideBar.jsx
@@ -8,6 +8,18 @@ function SideBar() {
         <img className="sidebar__avatar" src={avatar} alt="Default Avatar" />
         <p>User Name</p>
       </section>
+      {/* <section className="profile__sidebar"> */}
+      {/* <button> */}
+      {/* </button> */}
+      {/* </section> */}
+      <section className="settings__sidebar">
+        <button className="edit-profile">
+          <a className="btn_text">Edit Profile</a>
+        </button>
+        <button className="logout">
+          <a className="btn_text">Log Out</a>
+        </button>
+      </section>
     </div>
   );
 }
